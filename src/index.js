3221ab4923225979a7f53bcfc7d719695425719a
Matthieu <mail4thieu@gmail.com>
2020-10-26 10:12:15 +0100

    Remove test timeout

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import CreateMediaPage from './classes/CreateMediaPage';
 
 if (process && process.env && process.env.ENVIRONMENT === 'testing') {
-  setTimeout(() => {
-    const createMediaPage = new CreateMediaPage(
-      '#frame-tester',
-      process.env.TEST_API_KEY,
-      999,
-      {
-        publish: [
-          {
-            type: 'brandfolder',
-          },
-        ],
-      }
-    );
+  const createMediaPage = new CreateMediaPage(
+    '#frame-tester',
+    process.env.TEST_API_KEY,
+    1077,
+    {
+      publish: [
+        {
+          type: 'brandfolder',
+        },
+      ],
+    }
+  );
 
-    createMediaPage
-      .on('loaded', (pl) => console.log('loaded', pl))
-      .on('created', (media) => console.log('CREATED', media));
-  }, 4000);
+  createMediaPage
+    .on('loaded', (pl) => console.log('loaded', pl))
+    .on('created', (media) => console.log('CREATED', media));
 }
 
 export default {
